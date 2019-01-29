d15afb86c82c8ed17411cf87aa0fde4ea54a92eb
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2019-01-29 12:41:20 -0500

    complete auth abstraction and use passport as middleware

--- server/index.js
+++ server/index.js
@@ -1,36 +1,14 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys");
+require('./services/passport');
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
-
-// Passport should use a new instance of Google Strategy
-passport.use(
-    new GoogleStrategy(
-        {
-            clientID: keys.GOOGLE_CLIENT_ID,
-            clientSecret: keys.GOOGLE_CLIENT_SECRET,
-            callbackURL: '/auth/google/callback'
-        },
-            (accessToken) => {
-                console.log(accessToken);
-        }
-    )
-);
+// use passport as auth middleware
+authRoutes(app);
 
 app.get('/', (req, res) => {
     res.send({hi: 'there'});
 });
-
-app.get(
-    '/auth/google',
-    passport.authenticate('google', {
-        scope: ['profile', 'email']
-    })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
