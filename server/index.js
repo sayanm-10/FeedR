aea0e944cea8856d03ce18585d7560900930ad65
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2019-01-29 11:16:15 -0500

    extend gitignore to protect keys and env variables

--- server/index.js
+++ server/index.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
 const app = express();
+
+// Passport should use a new instance of Google Strategy
+passport.use(new GoogleStrategy());
 
 app.get('/', (req, res) => {
     res.send({hi: 'there'});
