e25c2c8281ec9622b398959400ac8aa534d5e164
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2019-01-29 14:20:44 -0500

    define User collection in MongoDB and store Google ID

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+require("./models/User");
 require('./services/passport');
 const authRoutes = require("./routes/authRoutes");
+
+
+// specify MongoDB connection
+mongoose.connect(keys.MONGO_URI);
 
 const app = express();
 // use passport as auth middleware
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,9 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
+const mongoose = require("mongoose");
+
+const User = mongoose.model("users");
 
 // Passport should use a new instance of Google Strategy
 passport.use(
@@ -11,10 +14,7 @@ passport.use(
             callbackURL: '/auth/google/callback'
         },
         (accessToken, refreshToken, profile, done) => {
-            console.log("Access Token:", accessToken);
-            console.log("Refresh Token:", refreshToken);
-            console.log("Profile:", profile);
-            console.log("Done! ", done);
+            new User({ googleId: profile.id }).save();
         }
     )
 );
