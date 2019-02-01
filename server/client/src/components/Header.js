ad4d4a9ffa6dc876a3b1b0cd0a0686f27b2845f5
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2019-02-01 12:07:15 -0500

    show available credit and style element

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -14,6 +14,7 @@ class Header extends Component {
             default:
                 return (<>
                         <li><Payment /></li>
+                        <li style={{margin: "0 10px"}}>{this.props.auth.credits} Credits</li>
                         <li><a href="/api/logout">Logout</a></li>
                     </>
                 );
